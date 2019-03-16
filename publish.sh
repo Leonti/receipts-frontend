062d93790c3dd7bcaf1386d9822cae3ba1555256
Leonti <prishelec@gmail.com>
2019-03-16 18:09:25 +1100

    Fix docker login

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t leonti/receipts-frontend:$version .
 docker push leonti/receipts-frontend
 git tag -a v$version -m 'new version $version'
